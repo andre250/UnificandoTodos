c8b9b7caf2449f8b7d8d5fcf7a4b88932d6233da
André Petridis <andrepetridis@gmail.com>
2017-01-31 17:05:43 -0200

    Sexto commit na branch

--- EntendendoFor.py
+++ EntendendoFor.py
@@ -13,4 +13,22 @@ livro3 = {'Titulo': 'Livro3',
 array_livros = [livro1, livro2, livro3]
 
 for i in array_livros:
-	print (i,'Titulo')
+	print (i['Titulo'])
+
+
+for i in [range(15)]:
+	print (i)
+
+l = [1,3,5,7,11]
+list_ret = []
+for i in l:
+	list_ret.append(i * 2)
+print (list_ret)
+
+# Isso é o equivalente do de cima
+l = [1,3,5,7,11]
+print ([i * 2 for i in l])
+
+# Possivel aplicação
+f = lambda x: [x * i for i in [1,3,5,7,11]]
+print (f(10))
